49d07b71353b4a78a7de2c57612b9e7490397a57
Steve Stanisic <kythyx@gmail.com>
2011-11-19 10:51:52 -0800

    moved stuff around

---
+++ COMP7615Asgn4Part2/COMP7615Asgn4Part2/Main.cs
@@ -0,0 +1,426 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace COMP7615Asgn4Part2
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Main : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // Maze
+        Maze maze;
+
+        // 3D
+        Matrix world, view, projection;
+
+        // Mouse Camera
+        MouseState originalMouse;
+        float fov = MathHelper.PiOver4;
+
+        // Switches
+        bool isMap;
+        bool isFog;
+        bool isDay;
+        bool isClip;
+        bool isFlash;
+
+        // Cube
+        List<Cube> cubes;
+        Model cubeModel;
+
+        MOB player;
+        MOB cartman;
+
+        // Cartman
+        Model cartmanModel;
+        int cartmanMoveFrames;
+
+        // Lighting
+        Vector3 ambientDay = new Vector3(0.7f, 0.7f, 0.7f);
+        Vector3 ambientNight = new Vector3(0.1f, 0.1f, 0.1f);
+        Vector3 diffuseDay = new Vector3(0.9f, 0.9f, 0.7f);
+        Vector3 diffuseNight = new Vector3(0.8f, 0.8f, 0.8f);
+        Vector3 diffuseDirection = new Vector3(0.1f, -1f, 0.1f);
+
+        // Flashlight
+        Texture2D flashTexture;
+        Vector2 flashPosition;
+
+        KeyboardState ksOld;
+        GamePadState gsOld;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            isMap = false;
+            isFog = false;
+            isClip = false;
+            isDay = true;
+
+            Mouse.SetPosition(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2);
+            originalMouse = Mouse.GetState();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Generate Maze
+            maze = new Maze(Content.Load<Texture2D>("Images/White"), Content.Load<Texture2D>("Images/Black"), Content.Load<Texture2D>("Images/Red"));
+
+            player = new MOB();
+            cartman = new MOB();
+
+            // Load Cartman
+            cartmanModel = Content.Load<Model>("cartman");
+            
+            // Load Cube Model
+            cubeModel = Content.Load<Model>("cube");
+
+            cubes = maze.CreateMaze(cubeModel);
+
+            ResetPosition();
+
+            // Flashlight
+            flashTexture = Content.Load<Texture2D>("Images/Flashlight");
+
+            flashPosition = new Vector2(this.Window.ClientBounds.Width / 2 - flashTexture.Width / 2,
+                                        this.Window.ClientBounds.Height / 2 - flashTexture.Height / 2);
+            
+            // Set up WVP Matrices
+            world = Matrix.Identity;
+            view = Matrix.Identity;
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(fov), (float)this.Window.ClientBounds.Width / (float)this.Window.ClientBounds.Height, 0.1f, 100f);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            maze.Update(new Vector2(-player.transX, -player.transZ));
+
+            HandleInput();
+            HandleMouse();
+            UpdateCamera();
+            EnemyMovement();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Handle mouse movement.
+        /// </summary>
+        private void HandleMouse()
+        {
+            MouseState ms = Mouse.GetState();
+
+            // Mouse Locking
+            if (ms != originalMouse)
+            {
+                float xDifference = (ms.X - originalMouse.X) / 2;
+                float yDifference = (ms.Y - originalMouse.Y) / 2;
+                player.angleX += 0.01f * xDifference;
+                player.angleY += 0.01f * yDifference;
+                Mouse.SetPosition(graphics.GraphicsDevice.Viewport.Width / 2, graphics.GraphicsDevice.Viewport.Height / 2);
+            }
+        }
+
+        /// <summary>
+        /// Handle Keyboard and Gamepad input since they are basically identical.
+        /// </summary>
+        private void HandleInput()
+        {
+            KeyboardState ks = Keyboard.GetState();
+            GamePadState gs = GamePad.GetState(PlayerIndex.One);
+         
+            // Close Program
+            if (ks.IsKeyDown(Keys.Escape) || gs.Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // Camera Angle
+            if (ks.IsKeyDown(Keys.Left) || gs.ThumbSticks.Right.X < 0)
+                player.angleX -= 0.03f;
+            if (ks.IsKeyDown(Keys.Right) || gs.ThumbSticks.Right.X > 0)
+                player.angleX += 0.03f;
+            if (ks.IsKeyDown(Keys.Up) || gs.ThumbSticks.Right.Y > 0)
+                player.angleY -= 0.03f;
+            if (ks.IsKeyDown(Keys.Down) || gs.ThumbSticks.Right.Y < 0)
+                player.angleY += 0.03f;
+
+            // Zoom / FOV
+            if (ks.IsKeyDown(Keys.Add) || gs.Triggers.Right > 0)
+                fov -= 0.1f;
+            if (ks.IsKeyDown(Keys.Subtract) || gs.Triggers.Left > 0)
+                fov += 0.1f;
+
+            // Toggle Map
+            if ((ks.IsKeyDown(Keys.M) && ksOld.IsKeyUp(Keys.M)) || (gs.Buttons.Start == ButtonState.Pressed && gsOld.Buttons.Start == ButtonState.Released))
+                isMap = !isMap;
+
+            // Toggle Fog
+            if ((ks.IsKeyDown(Keys.G) && ksOld.IsKeyUp(Keys.G)) || (gs.Buttons.A == ButtonState.Pressed && gsOld.Buttons.A == ButtonState.Released))
+                isFog = !isFog;
+
+            // Toggle Flashlight
+            if ((ks.IsKeyDown(Keys.F) && ksOld.IsKeyUp(Keys.F)) || (gs.Buttons.B == ButtonState.Pressed && gsOld.Buttons.B == ButtonState.Released))
+                isFlash = !isFlash;
+
+            // Toggle Day/Night
+            if ((ks.IsKeyDown(Keys.L) && ksOld.IsKeyUp(Keys.L)) || (gs.Buttons.X == ButtonState.Pressed && gsOld.Buttons.X == ButtonState.Released))
+                isDay = !isDay;
+
+            // Toggle Clipping
+            if ((ks.IsKeyDown(Keys.C) && ksOld.IsKeyUp(Keys.C)) || (gs.Buttons.Y == ButtonState.Pressed && gsOld.Buttons.Y == ButtonState.Released))
+                isClip = !isClip;
+
+            // Home
+            if ((ks.IsKeyDown(Keys.Home) && ksOld.IsKeyUp(Keys.Home)) || (gs.Buttons.LeftShoulder == ButtonState.Pressed && gsOld.Buttons.LeftShoulder == ButtonState.Released))
+                ResetPosition();
+
+            // Generate New Map
+            if ((ks.IsKeyDown(Keys.R) && ksOld.IsKeyUp(Keys.R)) || (gs.Buttons.RightShoulder == ButtonState.Pressed && gsOld.Buttons.RightShoulder == ButtonState.Released))
+            {
+                maze.GenerateMaze();
+                cubes = maze.CreateMaze(cubeModel);
+                ResetPosition();
+            }
+             
+            // Do Movement
+            if (ks.IsKeyDown(Keys.W) || gs.ThumbSticks.Left.Y > 0)
+                player.Move(Defs.Move.Forward, isClip, cubes);
+            if (ks.IsKeyDown(Keys.S) || gs.ThumbSticks.Left.Y < 0)
+                player.Move(Defs.Move.Backward, isClip, cubes);
+            if (ks.IsKeyDown(Keys.A) || gs.ThumbSticks.Left.X < 0)
+                player.Move(Defs.Move.Left, isClip, cubes);
+            if (ks.IsKeyDown(Keys.D) || gs.ThumbSticks.Left.X > 0)
+                player.Move(Defs.Move.Right, isClip, cubes);
+
+            // Set Previous KeyboardState
+            ksOld = ks;
+            gsOld = gs;
+        }
+
+        /// <summary>
+        /// Update the our view and projection matrices based on which way the player is facing.
+        /// </summary>
+        private void UpdateCamera()
+        {
+            Matrix R = Matrix.CreateRotationY(player.angleX) * Matrix.CreateRotationX(player.angleY) * Matrix.CreateRotationZ(player.angleZ);
+            Matrix T = Matrix.CreateTranslation(player.transX, 0, player.transZ);
+            
+            view = T * R;
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(fov), (float)this.Window.ClientBounds.Width / (float)this.Window.ClientBounds.Height, 0.1f, 100f);
+
+            // Reset Angles
+            if (player.angleX > 2 * Math.PI)
+                player.angleX = 0;
+
+            if (player.angleY > 2 * Math.PI)
+                player.angleY = 0;
+        }
+
+        /// <summary>
+        /// Move Cartman around the map.
+        /// </summary>
+        private void EnemyMovement()
+        {
+            bool change = true;
+
+            if (cartmanMoveFrames < 0)
+            {
+                cartmanMoveFrames = 40;
+
+                Random random = new Random();
+
+                int dir = maze.CheckCell(new Vector2(cartman.transX / 2, cartman.transZ / 2));
+
+                switch (dir)
+                {
+                    case (int)Defs.Direction.N:
+                        cartman.angleX = 2 * MathHelper.PiOver2;
+                        break;
+                    case (int)Defs.Direction.S:
+                        cartman.angleX = 4 * MathHelper.PiOver2;
+                        break;
+                    case (int)Defs.Direction.E:
+                        cartman.angleX = 3 * MathHelper.PiOver2;
+                        break;
+                    case (int)Defs.Direction.W:
+                        cartman.angleX = 1 * MathHelper.PiOver2;
+                        break;
+                    default:
+                        change = false;
+                        break;
+                }
+            }
+
+            if (change)
+                cartman.Move(Defs.Move.Forward, true, cubes);
+
+            cartmanMoveFrames--;
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            if (isDay)
+                GraphicsDevice.Clear(Color.SkyBlue);
+            else
+                GraphicsDevice.Clear(Color.Black);
+
+            // Render Cubes
+            foreach (Cube cube in cubes)
+            {
+                DrawModel(cube.Model, cube.Position, -(float)Math.PI, 0, 1);
+            }
+
+            Vector3 cartmanPosition = new Vector3(cartman.transX, -0.5f, cartman.transZ);
+            // Render Cartman
+            DrawModel(cartmanModel, cartmanPosition, -(float)MathHelper.PiOver2, -cartman.angleX, 0.1f);
+
+            // Flashlight
+            if (isFlash)
+            {
+                spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive);
+
+                spriteBatch.Draw(flashTexture, flashPosition, Color.White);
+
+                spriteBatch.End();
+
+                // Reset States for Rendering
+                GraphicsDevice.BlendState = BlendState.Opaque;
+                GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            }
+
+            if (isMap)
+            {
+                // Draw Map
+                spriteBatch.Begin();
+
+                maze.DrawMap(spriteBatch);
+
+                spriteBatch.End();
+
+                // Reset States for Rendering
+                GraphicsDevice.BlendState = BlendState.Opaque;
+                GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            }
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Draw the given model.
+        /// </summary>
+        /// <param name="model">The model to draw.</param>
+        /// <param name="position">The world position to draw it at.</param>
+        /// <param name="rotateOnX">The rotation about the X axis to apply.</param>
+        /// <param name="rotateOnY">The rotation about the Y axis to apply.</param>
+        /// <param name="scale">Scaling factor to apply.</param>
+        private void DrawModel(Model model, Vector3 position, float rotateOnX, float rotateOnY, float scale)
+        {
+            Matrix[] transformMat = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transformMat);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.LightingEnabled = true;
+                    effect.DirectionalLight0.Enabled = true;
+
+                    // Day/Night
+                    if (isDay)
+                    {
+                        effect.DirectionalLight0.DiffuseColor = diffuseDay;
+                        effect.AmbientLightColor = ambientDay;
+                    }
+                    else
+                    {
+                        effect.DirectionalLight0.DiffuseColor = diffuseNight;
+                        effect.AmbientLightColor = ambientNight;
+                    }
+
+                    effect.DirectionalLight0.Direction = diffuseDirection;
+
+                    // Fog
+                    if (isFog)
+                    {
+                        effect.FogStart = 0f;
+                        effect.FogEnd = 10f;
+                        effect.FogColor = new Vector3(1, 1, 1);
+                        effect.FogEnabled = true;
+                    }
+                    else
+                        effect.FogEnabled = false;
+
+                    Matrix matrixTrans = Matrix.CreateTranslation(position);
+                    Matrix matrixRot = Matrix.CreateRotationX(rotateOnX) * Matrix.CreateRotationY(rotateOnY);
+                    Matrix matrixScale = Matrix.CreateScale(scale);
+
+                    effect.World = transformMat[mesh.ParentBone.Index] * matrixScale * matrixRot * matrixTrans * world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
+        }
+
+        /// <summary>
+        /// Reset player and enemy to starting positions.
+        /// </summary>
+        private void ResetPosition()
+        {
+            fov = 70;
+            player.transX = 0;
+            player.transZ = -2;
+            player.angleX = MathHelper.PiOver2;
+            cartman.transX = (Defs.MapWidth - 1) * 2 - 2.5f;
+            cartman.transZ = (Defs.MapHeight - 2) * 2;
+            cartmanMoveFrames = 0;
+        }
+    }
+}
---
+++ COMP7615Asgn4Part2/COMP7615Asgn4Part2/Maze.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace COMP7615Asgn4Part2
+{
+    /// <summary>
+    /// This class contains the state and creation logic for the 2D Maze.
+    /// </summary>
+    class Maze
+    {
+        int[,] cells;
+        Texture2D whiteTex;
+        Texture2D blackTex;
+        Texture2D redTex;
+        Vector2 position;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="white">Texture to use for walkable map spaces.</param>
+        /// <param name="black">Texture to use for wall map spaces.</param>
+        /// <param name="red">Texture to use for current player space.</param>
+        public Maze(Texture2D white, Texture2D black, Texture2D red)
+        {
+            whiteTex = white;
+            blackTex = black;
+            redTex = red;
+
+            cells = new int[Defs.MapWidth, Defs.MapHeight];
+
+            GenerateMaze();
+        }
+
+        /// <summary>
+        /// Call this method to generate a new random maze of the size set by the default Map Width and Height.
+        /// </summary>
+        public void GenerateMaze()
+        {
+            Random random = new Random();
+
+            position = new Vector2(0, 1);
+
+            // Wall Everything
+            for (int w = 0; w < Defs.MapWidth; w++)
+            {
+                for (int h = 0; h < Defs.MapHeight; h++)
+                {
+                    cells[w, h] = 1;
+                }
+            }
+
+            // Potential Wall List
+            List<Vector2> cellList = new List<Vector2>();
+
+            // Open First Cell
+            cellList = Mark(1, 1, cellList);
+
+            while (cellList.Count > 0)
+            {
+                // Find Random Wall
+                int cell = random.Next(cellList.Count);
+
+                // Get Position of Wall
+                Vector2 nextCell = cellList[cell];
+
+                // Remove from list
+                cellList.RemoveAt(cell);
+
+                // Check if edge
+                if (nextCell.X == 0 || nextCell.X == Defs.MapWidth - 1
+                 || nextCell.Y == 0 || nextCell.Y == Defs.MapHeight - 1)
+                    continue;
+
+                // Check if theres wall on the other side
+                if(CheckNextCell(nextCell))
+                    cellList = Mark((int)nextCell.X, (int)nextCell.Y, cellList);
+            }
+
+            // Make sure Entrance and Exit are open
+            cells[0, 1] = 0;
+            cells[Defs.MapWidth - 1, Defs.MapHeight - 2] = 0;
+            cells[Defs.MapWidth - 2, Defs.MapHeight - 2] = 0;
+
+            // Open Random Paths
+            for (int i = 0; i < Defs.RandomPath; i++)
+            {
+                int x = random.Next(1, Defs.MapWidth - 2);
+                int y = random.Next(1, Defs.MapHeight - 2);
+
+                cells[x, y] = 0;
+            }
+        }
+
+        /// <summary>
+        /// Call this method to mark a given block as a Wall.
+        /// </summary>
+        /// <param name="x">The X position of the wall in tiles.</param>
+        /// <param name="y">The Y position of the wall in tiles.</param>
+        /// <param name="wallList">The list of walls to add it to.</param>
+        /// <returns>Returns the new list of walls.</returns>
+        private List<Vector2> Mark(int x, int y, List<Vector2> wallList)
+        {
+            // Remove Wall
+            cells[x, y] = 0;
+
+            // Add Potential Walls
+            if (cells[x - 1, y] == 1)
+                wallList.Add(new Vector2(x - 1, y));
+
+            if (cells[x + 1, y] == 1)
+                wallList.Add(new Vector2(x + 1, y));
+
+            if (cells[x, y - 1] == 1)
+                wallList.Add(new Vector2(x, y - 1));
+
+            if (cells[x, y + 1] == 1)
+                wallList.Add(new Vector2(x, y + 1));
+
+            return wallList;
+        }
+
+        private bool CheckNextCell(Vector2 currentCell)
+        {
+            int paths = 0;
+            int cx = (int)currentCell.X;
+            int cy = (int)currentCell.Y;
+
+            // Check Each Adjacent Cell
+            if (cells[cx + 1, cy] == 0)
+            {
+                paths++;
+            }
+
+            if (cells[cx - 1, cy] == 0)
+            {
+                paths++;
+            }
+
+            if (cells[cx, cy - 1] == 0)
+            {
+                paths++;
+            }
+
+            if (cells[cx, cy + 1] == 0)
+            {
+                paths++;
+            }
+
+            // Return true only if there is 1 or less adjacent paths
+            if (paths <= 1)
+                return true;
+
+            return false;
+        }
+
+        public int CheckCell(Vector2 currentCell)
+        {
+            Random random = new Random();
+
+            int cx = (int)Math.Round((double)currentCell.X);
+            int cy = (int)Math.Round((double)currentCell.Y);
+
+            List<int> directions = new List<int>();
+
+            // Check Each Adjacent Cell
+            if (0 < cx && cx < Defs.MapWidth && cy > 0 && cells[cx, cy - 1] == 0)
+                directions.Add((int)Defs.Direction.N);
+
+            if (0 < cx && cx < Defs.MapWidth && cy + 1 < Defs.MapHeight - 1 && cells[cx, cy + 1] == 0)
+                directions.Add((int)Defs.Direction.S);
+
+            if (0 < cy && cy < Defs.MapHeight && cx + 1 < Defs.MapWidth - 1 && cells[cx + 1, cy] == 0)
+                directions.Add((int)Defs.Direction.E);
+
+            if (0 < cy && cy < Defs.MapHeight && cx > 0 && cells[cx - 1, cy] == 0)
+                directions.Add((int)Defs.Direction.W);
+
+            if (directions.Count > 0)
+                return directions[random.Next(directions.Count)];
+            else
+                return -1;
+        }
+
+        /// <summary>
+        /// Call this method to draw the minimap on the screen.
+        /// </summary>
+        /// <param name="sb">The SpriteBatch to use to draw.</param>
+        public void DrawMap(SpriteBatch sb)
+        {
+            for (int w = 0; w < Defs.MapWidth; w++)
+            {
+                for (int h = 0; h < Defs.MapHeight; h++)
+                {
+                    if (cells[w, h] == 0)
+                        sb.Draw(whiteTex, new Vector2(w * 20, h * 20), Color.White);
+                    else
+                        sb.Draw(blackTex, new Vector2(w * 20, h * 20), Color.White);
+                }
+            }
+
+            sb.Draw(redTex, position * 20, Color.White);
+        }
+
+        /// <summary>
+        /// Call this method to generate a set of cube objects based on the current maze layout.
+        /// </summary>
+        /// <param name="model">The model to inject into the cubes.</param>
+        /// <returns>A list of cube objects.</returns>
+        public List<Cube> CreateMaze(Model model)
+        {
+            List<Cube> cubes;
+
+            // Get Maze Array
+            int[,] mazePos = cells;
+
+            cubes = new List<Cube>();
+
+            // Create 3D Maze
+            for (int width = 0; width < Defs.MapWidth; width++)
+            {
+                for (int height = 0; height < Defs.MapHeight; height++)
+                {
+                    if (mazePos[width, height] == 1)
+                        cubes.Add(new Cube(model, new Vector3(width * 2, 0, height * 2)));
+
+                    // Create Floor
+                    cubes.Add(new Cube(model, new Vector3(width * 2, -2, height * 2)));
+                }
+            }
+
+            return cubes;
+        }
+
+        /// <summary>
+        /// Call this function to update the player's current position on the minimap.
+        /// </summary>
+        /// <param name="mapPosition">The player's world position in world coordinates.</param>
+        public void Update(Vector2 mapPosition)
+        {
+            position = mapPosition / 2;
+        }
+
+        public int[,] Cells
+        {
+            get
+            {
+                return cells;
+            }
+        }
+    }
+}
